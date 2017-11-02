188bf2e4013f9b0d4383d6757cc5956db4baff1c
Yidan Wang <wangyidan621@gmail.com>
2017-11-02 20:55:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ function addToCart(item) {
  // write your code here
  let obj = {[item]:Math.floor(Math.random()*100)}
  cart.push(obj)
- console.log("${item} has been added to your cart.")
+ console.log(item + " has been added to your cart.")
  return cart
 }
 

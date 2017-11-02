d76c8a0208e7a6933c06b5b0d095092a85f889e6
Yidan Wang <wangyidan621@gmail.com>
2017-11-02 21:50:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -83,5 +83,6 @@ function placeOrder(cardNumber) {
     console.log("Sorry, we don't have a credit card on file for you.")
   } else{
     console.log("Your total cost is $"+ total() + `, which will be charged to the card ${cardNumber}.`)
+    cart = []
   }
 }
